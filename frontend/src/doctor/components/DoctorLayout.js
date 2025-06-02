b0205e321d0d69b7987fcfcaf0b343b780aac736
mytmatmong <167696515+mytmatmong@users.noreply.github.com>
2025-06-03 05:01:36 +0700

    Update DoctorLayout.js

--- frontend/src/doctor/components/DoctorLayout.js
+++ frontend/src/doctor/components/DoctorLayout.js
@@ -1,1 +1,16 @@
+import React from "react";
+import HeaderDoctor from "./DoctorHeader";
+import FooterDoctor from "./DoctorFooter";
+import "../styles/DoctorLayout.css";
 
+const DoctorLayout = ({ children }) => {
+    return (
+      <>
+        <HeaderDoctor />
+        <main>{children}</main>
+        <FooterDoctor />
+      </>
+    );
+  };
+  
+  export default DoctorLayout;
